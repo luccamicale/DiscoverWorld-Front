61b1082c6de9fd7a0159f9585d4cddefb958b2f1
luccamicale <luccamicale2015@gmail.com>
2023-11-21 01:05:37 -0300

    refactor: :zap: I created a new folders to move the files inside that

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
-import CityList from './City';
-import CityDetails from './CityDetails';
+import CityList from './Components/City/City';
+import CityDetails from './Components/CityDetails/CityDetails';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
